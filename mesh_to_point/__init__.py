7cebd500a8ff075df88e1582c11064108493a50c
Giorgio Mariani <giorgiomariani94@gmail.com>
2026-06-24 11:15:27 +0200

    Update package init file

--- mesh_to_point/__init__.py
+++ mesh_to_point/__init__.py
@@ -1,9 +1,14 @@
-from mesh_to_point.render import render_dataset
+from mesh_to_point.render import render_dataset, GlobalConfig
 from mesh_to_point.pointcloud import create_pointcloud_from_multiview
 from mesh_to_point.pointcloud.misc import create_pointcloud_figure
+from mesh_to_point.lights import read_light_config
+from mesh_to_point.camera import read_camera_config
 
 __all__ = [
     "render_dataset",
     "create_pointcloud_from_multiview",
     "create_pointcloud_figure",
+    "GlobalConfig",
+    "read_camera_config",
+    "read_light_config",
 ]
